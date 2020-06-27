3a05616a3e83ef1274a6bfdfb08d95b19afec405
Yuri Burk <yuriburk@gmail.com>
2020-06-26 21:49:39 -0300

    feature: Created SignIn tests

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
 
 import SignIn from '../../pages/SignIn';
 
+const mockedHistoryPush = jest.fn();
+
 jest.mock('react-router-dom', () => ({
-  useHistory: jest.fn(),
+  useHistory: () => ({
+    push: mockedHistoryPush,
+  }),
   Link: ({ children }: { children: React.ReactNode }) => children,
 }));
 
-describe('SignIn Page', () => {
-  it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
+jest.mock('../../hooks/auth', () => ({
+  useAuth: () => ({
+    signIn: jest.fn(),
+  }),
+}));
 
-    debug();
+describe('SignIn Page', () => {
+  it('should be able to sign in', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '1234' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() =>
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard'),
+    );
   });
 });
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -23,6 +23,8 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const history = useHistory();
 
   const { signIn } = useAuth();
   const { addToast } = useToast();
@@ -46,6 +48,8 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password,
         });
+
+        history.push('/dashboard');
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
           const errors = getValidationErrors(error);
